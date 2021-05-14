9fa05575ee31cbf939737385862ad81103a88210
itsmohitj <itsmohitj@gmail.com>
2021-05-14 23:52:11 +0530

    List can have integers as well as characters now

--- BinaryTree/main.py
+++ BinaryTree/main.py
@@ -1,18 +1,13 @@
 import traversal 
+import createTree
+
 class Node:
     def __init__(self,data):
         self.data = data
         self.left = self.right = None
 
-    def insertLevelOrder(arr, root, i, n):
-        if i < n:
-            temp = Node(arr[i])
-            root = temp
-            root.left = Node.insertLevelOrder(arr, root.left, 2 * i + 1, n)
-            root.right = Node.insertLevelOrder(arr, root.right, 2 * i + 2, n)
-        return root
 
 if __name__ == '__main__':
-    lst = list(map(int,input().split()))
-    root = Node.insertLevelOrder(lst, None, 0, len(lst))
+    lst = list(map(lambda x : x if type(x) is int else x, input().split()))
+    root = createTree.createLevelOrder(lst, None, 0, len(lst))
     traversal.printInorder(root)
